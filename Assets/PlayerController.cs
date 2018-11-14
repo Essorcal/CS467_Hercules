f915b75314008ee97260870ab5b54378e3872be6
bmdoil <bmdoil1886@gmail.com>
2018-11-14 18:18:21 -0500

    Adding necessary meta files for Combat, Controller, Manager. Additional cleanup

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CapsuleCollider2D playerCollider;
+    public float moveSpeed = 1.05f;
+
+    private Enemy enemyScript;
+
+    private RaycastHit hit;
+    private Ray ray;
+    public float rayDistance = 4;
+
+    private void Start()
+    {
+        playerCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+}
