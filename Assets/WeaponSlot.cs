26ed4e79855f2870db4ed08de611e08961652ecd
Unknown <lenoja@oregonstate.edu>
2018-11-23 18:47:38 -0700

    Reapplied Stash

---
+++ Assets/WeaponSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour {
+
+    public Weapon currentWeapon;
+
+	void Start () {
+        Instantiate(currentWeapon.weaponPreb, new Vector3(0,0,0), Quaternion.identity, gameObject.transform);
+	}
+	
+	void Update () {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+}
